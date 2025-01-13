58487f0bdf4fa9f4427a571698067ff4e9fdb2a6
yassinsl <slawiyassin152@gmail.com>
2025-01-13 09:30:09 +0000

    UP

--- Push_swap_last/src/main.c
+++ Push_swap_last/src/main.c
@@ -1,17 +1,15 @@
 #include "push_swap.h"
-
-void	ft_free_array(char **arr)
+void ft_free_array(char **arr)
 {
-	int	i;
-
-	i = 0;
-	while (arr[i])
-	{
-		free(arr[i]);
-		i++;
-	}
-	free(arr);
+    int i = 0;
+    while (arr[i])
+    {
+        free(arr[i]);
+        i++;
+    }
+    free(arr);
 }
+
 int is_check(char *arr)
 {
     while (*arr)
@@ -36,23 +34,22 @@ int is_duplicate(t_list *list, int number)
 
 void error(void)
 {
-    write(2, "error\n", ft_strlen("error\n"));
+    write(2, "error\n", 6);
     exit(EXIT_FAILURE);
 }
 
 static t_list *first_stack(char **arr, int len_size)
 {
-    int i, num;
+    int i = -1, num;
     t_list *list = NULL;
     t_list *tmp;
 
-    for (i = 0; i < len_size; i++)
+    while (++i < len_size)
     {
         if (ft_strchr(arr[i], ' '))
         {
             char **args = ft_split(arr[i], ' ');
             int j = 0;
-
             while (args[j])
             {
                 num = ft_atoi(args[j]);
@@ -69,11 +66,10 @@ static t_list *first_stack(char **arr, int len_size)
             ft_free_array(args);
             continue;
         }
-
         num = ft_atoi(arr[i]);
         if (!is_check(arr[i]) || !is_duplicate(list, num))
         {
-          ft_lstclear(&list);
+            ft_lstclear(&list);
             error();
         }
         tmp = ft_lstnew(num);
@@ -81,24 +77,110 @@ static t_list *first_stack(char **arr, int len_size)
     }
     return list;
 }
-#include <stdio.h>
+
+int get_prev(t_list *stack_a, int position)
+{
+    int currentIndex = 1;
+    while (stack_a != NULL)
+    {
+        if (currentIndex == position)
+            return stack_a->content;
+        stack_a = stack_a->next;
+        currentIndex++;
+    }
+    return -1;
+}
+
+void check_position_prev(t_prev *prev, int len, t_list *stack_a)
+{
+    prev->position_major = (3 * len) / 4;
+    prev->position_minor = len / 4;
+    prev->major = get_prev(stack_a, prev->position_major);
+    prev->minor = get_prev(stack_a, prev->position_minor);
+}
+
+t_list *pop_from_a(t_list **stack_a)
+{
+    t_list *top = *stack_a;
+    if (!top)
+        return NULL;
+    *stack_a = (*stack_a)->next;
+    top->next = NULL;
+    return top;
+}
+
+void push_to_b(t_list **stack_b, t_list *top)
+{
+    if (top)
+    {
+        ft_lstadd_front(stack_b, top);
+        ft_printf("pb\n");
+    }
+}
+
+void rotate_a(t_list **stack_a)
+{
+    if (!stack_a || !*stack_a || !(*stack_a)->next)
+        return;
+    t_list *first = *stack_a;
+    t_list *last = *stack_a;
+    while (last->next)
+        last = last->next;
+    *stack_a = first->next;
+    first->next = NULL;
+    last->next = first;
+    ft_printf("ra\n");
+}
+
+void change_partition(t_list **stack_a, t_list **stack_b, t_prev *prev)
+{
+    t_list *node = *stack_a;
+
+    while (node)
+    {
+        if (node->content < prev->minor || node->content > prev->major)
+            push_to_b(stack_b, pop_from_a(stack_a));
+        else
+            rotate_a(stack_a);
+        node = *stack_a;
+    }
+}
+
+void recursive_sort(t_list **stack_a, t_list **stack_b, int len)
+{
+    if (len <= 1)
+        return;
+
+    t_prev *prev = malloc(sizeof(t_prev));
+    if (!prev)
+        return;
+    check_position_prev(prev, len, *stack_a);
+    change_partition(stack_a, stack_b, prev);
+
+    int len_low = prev->position_minor;
+    int len_mid = prev->position_major - prev->position_minor;
+    int len_high = len - prev->position_major;
+
+    recursive_sort(stack_a, stack_b, len_low);
+    recursive_sort(stack_a, stack_b, len_mid);
+    recursive_sort(stack_a, stack_b, len_high);
+
+    free(prev);
+}
+
+void three_way_quick_sort(t_list *stack_a, t_list *stack_b, int len)
+{
+    recursive_sort(&stack_a, &stack_b, len);
+}
+
 int main(int ac, char **av)
 {
     t_list *first_stack_a;
-    t_list *second_stack = NULL;
-    (void)second_stack;
+    t_list *second_stack_b = NULL;
+
     if (ac < 2)
         error();
-
     first_stack_a = first_stack(&av[1], ac - 1);
-    t_list *tmp = first_stack_a;
-    while(first_stack_a)
-  {
-    printf("%d", first_stack_a->content);
-    first_stack_a = first_stack_a->next;
-  }
-    ft_lstclear(&tmp);
-
+    three_way_quick_sort(first_stack_a, second_stack_b, ft_lstsize(first_stack_a));
     return 0;
 }
-
--- Push_swap_last/src/push_swap.h
+++ Push_swap_last/src/push_swap.h
@@ -2,6 +2,12 @@
 #define PUSH_SWAP_H
 
 #include "../Libft/libft.h"
-
+typedef struct s_prev
+{
+    int position_major;
+    int position_minor;
+    int major;
+    int minor;
+} t_prev;
 
 #endif
